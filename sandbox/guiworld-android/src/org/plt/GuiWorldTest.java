37dac498212c66807d745b32b80cf04a19bd21e0
Zhe Zhang <zhe@cs.brown.edu>
2009-04-04 18:48:57 -0400

    Added comments to code.

--- sandbox/guiworld-android/src/org/plt/GuiWorldTest.java
+++ sandbox/guiworld-android/src/org/plt/GuiWorldTest.java
@@ -22,20 +22,21 @@ public class GuiWorldTest extends Activity {
 		Gui gui = new Col(new Gui[] {
 
 		new Row(new Gui[] { new Message(new WorldTransformer() {
+			// returns: content of Message
 			public Object transform(Object world) {
 				return "Hello world";
 			}
 		}),
 
 		new Message(new WorldTransformer() {
-			// label
+			// returns: content of Message
 			public Object transform(Object world) {
 				return "The World says: " + world.toString();
 			}
 		}),
 
 		new org.plt.guiworld.Button(new WorldTransformer() {
-			// label
+			// returns: label of Button
 			public Object transform(Object world) {
 				return "says: " + world.toString();
 			}
@@ -46,16 +47,16 @@ public class GuiWorldTest extends Activity {
 		}) }),
 
 		new org.plt.guiworld.TextField(new WorldTransformer() {
+			// returns: content of TextField
 			public Object transform(Object world) {
-				return "init text in text field: " + world.toString();
+				return world.toString();
 			}
 		}, new WorldAndObjectTransformer() {
+			// obj is the content of TextField
 			public Object transform(Object world, Object obj) {
 				try {
-					int val = Integer.parseInt(obj.toString());
-					Integer newWorld = new Integer(((Integer) world).intValue()
-							+ val);
-					return newWorld;
+					return Integer.parseInt(obj.toString());
+
 				} catch (NumberFormatException e) {
 					return new Integer(0);
 				}
@@ -63,71 +64,76 @@ public class GuiWorldTest extends Activity {
 		}),
 
 		new DropDown(new WorldTransformer() {
+			// returns: content of would-be-selected item
 			public Object transform(Object world) {
 				Integer count = (Integer) world;
 				return Integer.toString(count);
 			}
 		}, new WorldTransformer() {
+			// returns: list of values to be shown in this DropDown
 			public Object transform(Object world) {
 				Integer count = (Integer) world;
 				org.plt.types.List items = org.plt.types.Empty.EMPTY;
-				for(int i = Math.max(count.intValue(), 20);
-				    i >= 1; i--) {
-				    items = new org.plt.types.Pair(String.valueOf(i),
-								   items);
+				for (int i = Math.max(count.intValue(), 20); i >= 1; i--) {
+					items = new org.plt.types.Pair(String.valueOf(i), items);
 				}
 				return items;
 			}
 		}, new WorldAndObjectTransformer() {
+			// obj is the string content of selected item
 			public Object transform(Object world, Object obj) {
-			    return new Integer(Integer.parseInt((String)obj));
-				// return obj;
-			    //  return world;
+				return new Integer(Integer.parseInt((String) obj));
 			}
 		}),
 
 		new Slider(new Integer(20), new Integer(50), new WorldTransformer() {
+			// returns: the would-be progress value in Slider
 			public Object transform(Object world) {
 				return Integer.parseInt(world.toString());
 			}
 		}, new WorldAndObjectTransformer() {
+			// obj is the current progress value in Slider GUI
 			public Object transform(Object world, Object obj) {
 				return Integer.parseInt(obj.toString());
 			}
 		}),
 
 		new org.plt.guiworld.CheckBox(new WorldTransformer() {
+			// returns: the label of CheckBox 
 			public Object transform(Object world) {
 				return "check: " + world.toString();
 			}
 		}, new WorldTransformer() {
+			// returns: the would-be value of CheckBox
 			public Object transform(Object world) {
-			    return Integer.parseInt(world.toString()) % 2 == 0 ?
-				org.plt.types.Logic.TRUE : 
-				org.plt.types.Logic.FALSE;
+				return Integer.parseInt(world.toString()) % 2 == 0 ? org.plt.types.Logic.TRUE
+						: org.plt.types.Logic.FALSE;
 			}
 		}, new WorldAndObjectTransformer() {
+			// obj is the logic value whether the checkbox is checked
 			public Object transform(Object world, Object obj) {
-			    if (((org.plt.types.Logic) obj).isTrue()) {
-				if (Integer.parseInt(world.toString()) % 2 == 0)
-				    return world;
-				else
-				    return new Integer(Integer.parseInt(world.toString()) - 1);
-			    } else {
-				if (Integer.parseInt(world.toString()) % 2 == 0)
-				    return new Integer(Integer.parseInt(world.toString()) - 1);
-				else
-				    return world;
-			    }
+				if (((org.plt.types.Logic) obj).isTrue()) {
+					if (Integer.parseInt(world.toString()) % 2 == 0)
+						return world;
+					else
+						return new Integer(
+								Integer.parseInt(world.toString()) - 1);
+				} else {
+					if (Integer.parseInt(world.toString()) % 2 == 0)
+						return new Integer(
+								Integer.parseInt(world.toString()) - 1);
+					else
+						return world;
+				}
 			}
 		}),
 
 		new BoxGroup(new WorldTransformer() {
+			// returns: label of BoxGroup
 			public Object transform(Object world) {
 				return "BoxGroup: " + world.toString();
 			}
 		}, new Message(new WorldTransformer() {
-			// label
 			public Object transform(Object world) {
 				return "Message in BoxGroup: " + world.toString();
 			}
